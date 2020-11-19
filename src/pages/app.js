0dc56a27b65b1b80e631d371ddde73ad9cba5981
Insaf Almukhametov <insaf_13@mail.ru>
2020-11-19 09:19:14 +0300

    Add: eject & webpack build

--- src/pages/app.js
+++ src/pages/app.js
@@ -13,67 +13,79 @@ import Error from "./error";
 import { CITY_NAME } from "../config";
 
 const Container = styled.div`
-  height: 100vh;
-  width: 100%;
+    height: 100vh;
+    width: 100%;
 `;
 
 const App = () => {
-  const slug = CITY_NAME;
-  const { data, loading, error } = useQuery(CITY_QUERY, {
-    variables: { slug }
-  });
-  const { dispatch } = useContext(StateContext);
+    const slug = CITY_NAME;
+    const { data, loading, error } = useQuery(CITY_QUERY, {
+        variables: { slug },
+    });
+    const { dispatch } = useContext(StateContext);
 
-  useEffect(() => {
-    const handleState = (type, payload) => dispatch({ type, payload });
+    useEffect(() => {
+        const handleState = (type, payload) => dispatch({ type, payload });
 
-    data &&
-      handleState(
-        "CHANGE_TERRITORY",
-        data?.cities?.[0]?.territories?.[0]?.slug
-      );
-    data?.cities?.[0]?.theme &&
-      handleState("CHANGE_THEME", data?.cities?.[0]?.theme);
-  }, [dispatch, data]);
+        data &&
+            handleState(
+                "CHANGE_TERRITORY",
+                data?.cities?.[0]?.territories?.[0]?.slug
+            );
+        data?.cities?.[0]?.theme &&
+            handleState("CHANGE_THEME", data?.cities?.[0]?.theme);
+    }, [dispatch, data]);
 
-  const { title } = data?.cities?.[0] ?? {};
+    const { title } = data?.cities?.[0] ?? {};
 
-  return (
-    <Router>
-      {data && (
-        <Helmet>
-          <meta name="description" content={title} />
-          <meta property="og:title" content={title} />
-          <meta property="og:type" content="website" />
-          {data?.cities?.[0]?.background?.url && (
-            <meta
-              property="og:image"
-              content={data?.cities?.[0]?.background?.url}
-            />
-          )}
-          <title>{title}</title>
-        </Helmet>
-      )}
-      <Container>
-        <Header {...{ title }} />
-        {error && <Error message={error?.message} />}
-        {data && (
-          <Switch>
-            <Route
-              path="/"
-              exact
-              render={props => <Main {...{ data, title, loading, ...props }} />}
-            />
-            <Route
-              path="/form"
-              exact
-              render={props => <Survey {...{ title, ...props }} />}
-            />
-          </Switch>
-        )}
-      </Container>
-    </Router>
-  );
+    return (
+        <Router
+            basename={
+                new RegExp(process.env.REACT_APP_CITY_NAME).test(
+                    process.env.PUBLIC_URL
+                )
+                    ? `/${process.env.REACT_APP_CITY_NAME}`
+                    : undefined
+            }
+        >
+            {data && (
+                <Helmet>
+                    <meta name="description" content={title} />
+                    <meta property="og:title" content={title} />
+                    <meta property="og:type" content="website" />
+                    {data?.cities?.[0]?.background?.url && (
+                        <meta
+                            property="og:image"
+                            content={data?.cities?.[0]?.background?.url}
+                        />
+                    )}
+                    <title>{title}</title>
+                </Helmet>
+            )}
+            <Container>
+                <Header {...{ title }} />
+                {error && <Error message={error?.message} />}
+                {data && (
+                    <Switch>
+                        <Route
+                            path="/"
+                            exact
+                            render={(props) => (
+                                <Main {...{ data, title, loading, ...props }} />
+                            )}
+                        />
+                        <Route
+                            path="/form"
+                            exact
+                            render={(props) => (
+                                <Survey {...{ title, ...props }} />
+                            )}
+                        />
+                    </Switch>
+                )}
+            </Container>
+        </Router>
+    );
 };
 
 export default App;
